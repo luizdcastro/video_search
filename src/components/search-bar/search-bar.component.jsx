f0ee341c28a4c4afa7c2780534720b9ccfdcf220
Luiz Castro <lfernando.cwb@gmail.com>
2020-06-10 15:21:25 -0300

    added search-bar component

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 
+import "./search-bar.styles.css";
+
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("pressed");
+  };
+
   render() {
-    return <div>Search Bar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="search-bar--input ui fluid action input">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+            <button className="ui icon button">
+              <i className="search icon"></i>
+            </button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
