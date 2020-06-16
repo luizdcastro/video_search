db378df5469fb29c23dfb2e9c4289ef90bdc9c4c
Luiz Castro <lfernando.cwb@gmail.com>
2020-06-15 22:01:28 -0300

    error 403 update

--- src/components/video-detail/video-detail.component.jsx
+++ src/components/video-detail/video-detail.component.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 
-const VideoDetail = ({ video }) => {
+const VideoDetail = ({ video, error }) => {
   if (!video) {
     return (
-      <div className="error ui container centered">
-        <h2 className="ui icon header">
-          <div className="divider content">
-            HTTP Error 403:
-            <div className="sub header">
-              Sorry the YouTube API daily limit has been reached
-            </div>
-            <div className="sub header">
-              The service should be available again within 24 hours.
-            </div>
-            <div className="ui divider">
-              <img
-                className="ui big image"
-                alt="naruto sad"
-                src="https://media.giphy.com/media/4NuAILyDbmD16/giphy.gif"
-              />
-            </div>
-          </div>
-        </h2>
+      <div className="ui center aligned icon header">
+        <h2 className="ui medium header">HTTP Error 403:</h2>
+        <h3 className="sub header">
+          Sorry, the YouTube API daily limit has been reached.
+        </h3>
+
+        <h3 className="sub header">
+          The service will be available again within 24 hours.
+        </h3>
+        <br />
+        <div>
+          <img
+            className="ui big centered image"
+            alt="naruto sad"
+            src="https://media.giphy.com/media/4NuAILyDbmD16/giphy.gif"
+          />
+        </div>
       </div>
     );
   }
